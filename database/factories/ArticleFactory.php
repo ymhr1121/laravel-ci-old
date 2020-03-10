d06bcf1a383b0ab9cd41b44567ba360972519044
ymhr1121 <ymhr1121@gmail.com>
2020-03-10 01:20:33 +0000

    2-x テスト作成

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Article;
+use Faker\Generator as Faker;
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(50),
+        'body' => $faker->text(500),
+        'user_id' => function() {
+            return factory(App\User::class);
+        },
+    ];
+});
